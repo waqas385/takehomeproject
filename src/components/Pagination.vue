db814a70cce01ebb6afe69c8d01d907729b41072
Waqas Ahmed <waqas385@gmail.com>
2024-02-15 11:41:35 +0500

    Updated select component UI and its usage

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,15 +1,117 @@
 <script setup lang="ts">
 import {ref} from 'vue';
-import PageCount from './PageCount.vue';
+import Select from './Select.vue';
 
-const itemsPerPage = ref(10);
+
+const noOfItemsPerPage = ref([
+  {
+    text: 10,
+    value: 10,
+    selected: true
+  },
+  {
+    text: 25,
+    value: 25,
+    selected: false
+  },
+  {
+    text: 50,
+    value: 50,
+    selected: false
+  },
+  {
+    text: 100,
+    value: 100,
+    selected: false
+  }
+]);
+
+const sortBy = ref([
+  {
+    text: 'A-Z',
+    value: 'asc',
+    selected: true
+  },
+  {
+    text: 'Create date',
+    value: 'create_date',
+    selected: false
+  },
+  {
+    text: 'Last modified',
+    value: 'update_date',
+    selected: false
+  }
+]);
+
+const pages = ref([
+{
+    text: '1 of 10',
+    value: 1,
+    selected: true
+  },
+  {
+    text: '2 of 10',
+    value: 2,
+    selected: false
+  },
+  {
+    text: '3 of 10',
+    value: 3,
+    selected: false
+  },
+  {
+    text: '4 of 10',
+    value: 4,
+    selected: false
+  },
+  {
+    text: '5 of 10',
+    value: 5,
+    selected: false
+  },
+  {
+    text: '6 of 10',
+    value: 6,
+    selected: false
+  },
+  {
+    text: '7 of 10',
+    value: 7,
+    selected: false
+  },
+  {
+    text: '8 of 10',
+    value: 8,
+    selected: false
+  },
+  {
+    text: '9 of 10',
+    value: 9,
+    selected: false
+  },
+  {
+    text: '10 of 10',
+    value: 10,
+    selected: false
+  },
+])
+
+function onNoOfItemsPerPageSelection(event) {
+  console.log(event);
+}
+
 </script>
 <template>
-  <div class="px-3 flex justify-between">
-    <div class="py-4">Page <b>1</b> of <b>10</b></div>
-    <div class="py-4">Sort by <b>Sort Order</b></div>
+  <div class="px-3 flex justify-between pt-3">
     <div>
-      <PageCount :items-per-page="itemsPerPage" />
+      <Select :options="pages" label="Page"/>
+    </div>
+    <div>
+      <Select :options="sortBy" label="Sort by" />
+    </div>
+    <div>
+      <Select :options="noOfItemsPerPage" @change=""/>
     </div>
   </div>
 </template>
---
+++ src/components/Select.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import {defineEmits} from 'vue';
+
+interface SelectOption {
+  text: string | number,
+  value: string | number,
+  selected?: boolean
+}
+
+const emit = defineEmits(['change']);
+
+defineProps({
+  options: {
+    type: Array<SelectOption>
+  },
+  label: {
+    type: String
+  }
+});
+
+function doSelect(selectedOption) {
+  console.log(selectedOption, selectedOption.target.value);
+  emit('change', selectedOption)
+}
+</script>
+<template>
+  <div class="flex justify-items-center">
+    <div v-if="label" class="shrink-0 pr-2">
+      <label>{{ label }}</label>
+    </div>
+    <select @change="doSelect" class="pl-2 rounded-md">
+      <option
+        v-for="option in options"
+        :value="option.value"
+        :selected="option.selected"
+      >{{ option.text }}</option>
+    </select>
+  </div>
+</template>
+<style scoped>
+select {
+  height: 24px;
+  width: 100%;
+  background: transparent;
+  font-weight: 700;
+  &:hover {
+    background-color: #fff;
+  }
+}
+</style>
